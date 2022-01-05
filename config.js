07ed16328c237801cc88e3346ed6726fc1a625cb
FODZERD <96115214+FODZERD@users.noreply.github.com>
2022-01-05 13:40:31 +0700

    update

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "bạn có 1 tin nhắn🧡!",
+    introTitle: 'Tên crush',
+    introDesc: `xin chào bạn`,
+    btnIntro: 'click vào đây :3',
+    title: 'cơn gió nào đã thổi bạn vào chốn này? o~o',
+    desc: 'thôi thì đã ghé rồi thì chúng ta làm 1 cặp đi nhé?',
+    btnYes: 'Me too ^^ 🧡',
+    btnNo: 'Không nha :3',
+    question: '1 vài lờn nhắn nhủ :3',
+    btnReply: 'Gửi cho bạn 🧡🧡',
+    reply: 'LOVE YOU TO THE MOON AND BACK ❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️',
+    mess: 'Mình cũng vậy!😘😘',
+    messDesc: 'mình sẽ chia sẽ thêm thông tin về mình:3',
+    btnAccept: 'Okiiiii lun ❤️',
+    messLink: 'https://facebook.com/DesignerDSP' 
+}
